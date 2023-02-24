a3f2b795aada409ed582d6b258849692cfbc0448
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-24 15:56:00 -0700

    create  initialize and name methods

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,16 @@
 class Author
+    attr_reader :author_details, :first_name, :last_name
 
+  def initialize(author_details)
+    @first_name = author_details[:first_name]
+    @last_name = author_details[:last_name]
+    @author_details = author_details
+  end
+
+  def name
+    names = [] 
+    names << @first_name
+    names << @last_name
+    names.join(" ")
+  end
 end
